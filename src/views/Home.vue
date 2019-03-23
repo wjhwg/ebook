6611f8f63a5ea9915fdcd581520027b59c0f66e5
1466619197@qq.com <1466619197@qq.com>
2019-03-23 22:48:22 +0800

    ebook init

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div id="read"></div>
+  </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+global.ePub = Epub
+export default {
+  name: 'home',
+  mounted() {
+    this.book = new Epub('/2017_Book_InterdisciplinaryPerspectivesO.epub')
+    this.book.renderTo('read',{
+      width: window.innerWidth,
+      height: window.innerHeight
+    }).display();
+  },
+}
+</script>
